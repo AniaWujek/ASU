15bf0174e850265b591c19767a1f3faaed94752c
Anna <aniawujek@gmail.com>
2015-01-17 22:12:43 +0100

    password repaired

--- users.pl
+++ users.pl
@@ -63,12 +63,12 @@ if ($create ne '') {
     
     #dajemy mu haslo
     if($passwd ne '') {
-        my $pass = `openssl passwd -crypt $passwd`;
+        my $pass = chomp(`openssl passwd -crypt $passwd`);
         $command = '-p ' . $pass . ' ' . $command;
     }
     elsif ($randpasswd) {
         my $pass = mkpasswd();
-        $pass = `openssl passwd -crypt $pass`;
+        $pass = chomp(`openssl passwd -crypt $pass`);
         say 'Twoje haslo to ' . $pass;
         $command = '-p ' . $pass . ' ' . $command;
     }
